02979bbdad6185d59763b3b0a20eb86599415f50
whiteboarddan <macnish.daniel@gmail.com>
2016-11-14 21:31:55 +1100

    add no internet flag

--- plantClass.py
+++ plantClass.py
@@ -10,8 +10,10 @@ class Plant:
         self.__duration = 10
 
 def main():
+    #set no internet to true to load data from file instead of acquiring from server
+    NO_INTERNET = True
     trfc = Traffic()
-    t = trfc.update(True)
+    t = trfc.update(NO_INTERNET)
     print(t)
 
 if __name__ == "__main__":
