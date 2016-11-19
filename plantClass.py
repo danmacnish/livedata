b21231563c011702f4265e94a2bff6749d7d7455
whiteboarddan <macnish.daniel@gmail.com>
2016-11-19 22:52:29 +1100

    update dummy class

--- plantClass.py
+++ plantClass.py
@@ -10,18 +10,31 @@ class Plant:
     def __init__(self):
         #turns pump on and off @ relay 1
         self.__pump = Pump(1)
-        self.pumpDuration = 5 #num seconds to turn the pump on for
+        self.__soil = moistureSensor()
+        self.__K = 50
+        self.__Heathly = 600 #moisture level when plant is healthy. moisture measurement ranges from 420 (saturated) to 810 (totally dry)
 
-    def calcPumpDuration(self):
-        #calculate pump duration based on error between current moisture and traffic data
-        self.__duration = 10
+    def calcPumpDuration(self, traffic):
+        #calculate how wilted we want the plant to be based on the current traffic data, using PID to control soil moisture
+        #first calculate the moisture setpoint based on the current traffic (more congestion -> drier soil)
+        moistureSP = self.__Healthy + self.__K * traffic
+        #Limit value within min/max of moisture SP
+        moistureSP = self.__clamp(moistureSP, self.__soil.getMin(), self.__soil.getMax())
+        #calculate error term
+        error = moistureSP - self.__soil.getMoisture()
+        print('moisture error is: ',error)
+
+    #clamp function to limit values
+    def __clamp(self,val, min, max):
+        return max(min(max, val), min)
+
 
 def main():
     #set no internet to true to load data from file instead of acquiring from server
     NO_INTERNET = False
     data = Queue.Queue()
     try:
-        thread.start_new_thread(getData, (data, NO_INTERNET, 5))
+        thread.start_new_thread(getData, (data, NO_INTERNET, 10))
         thread.start_new_thread(controlPlant, (data,))
     except:
         print('could not create threads')
@@ -34,6 +47,7 @@ def getData(queue ,internet, pollPeriod):
     trfc = Traffic()
     trfc.connectToServer(internet)
     while True:
+        #traffic.update returns the average delay in minutes for the area. 0 = normal free flowing traffic, -ve is faster than usual, +ve is slower traffic
         t = trfc.update(internet)
         #push new delay to queue
         queue.put(t)
@@ -45,13 +59,14 @@ def getData(queue ,internet, pollPeriod):
 
 def controlPlant(queue):
     plant = Plant()
+    trafficDelay = 0 #initialised to no delay
     while True:
         print('getting data from queue...')
         try:
             trafficDelay = queue.get()
         except Queue.Empty:
             print('queue empty')
-
+        plant.calcPumpDuration(trafficDelay)
 
 if __name__ == "__main__":
     sys.exit(main())
